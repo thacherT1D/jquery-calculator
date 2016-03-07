c682c3a4ac505e7079e002b40fa94bcb3b8f8d7f
THACHER <sophie.thacher@gmail.com>
2016-03-06 21:40:25 -0800

    small tweaks and clean up

--- js/main.js
+++ js/main.js
@@ -11,19 +11,15 @@ var result;
     var number = $(this).text();
     $('#screen').append(number);
     fullNumber = $('#screen').text();
-    console.log(fullNumber);
   });
 
 //Operators
   $('.operator:not(#calc, #cancel)').on('click', function(){
     var operator = $(this).text();
-    console.log(operator);
 
     if(operator === 'x'){
-      // actualOperator = '*';
       result = operator.replace(/x/g, '*');
       $('#screen').append(' ' + result + ' ');
-
     }
   //   else if(operator === &divide;){
   //    result = operator.replace(/\u00F7/g, '/');
@@ -48,76 +44,7 @@ var result;
   });
 //Clear
   $('#cancel').on('click', function(){
-    // console.log('cancel');
-    // console.log(this.innerHTML);
     $('#screen').empty();
   });
 
 });
-
-
-
-
-
-// $(document).ready(function(){
-//
-//   var currentTotal = 0;
-//   var secondNumber;
-//   var total;
-//
-//   function pickNumber(event){
-//     var number = this.innerHTML;
-//     if(currentTotal === 0) {
-//       currentTotal = number;
-//       console.log(currentTotal);
-//       $('#screen').html(currentTotal);
-//
-//     } else {
-//       secondNumber = number;
-//       $('#screen').html(secondNumber);
-//       total = +currentTotal + +secondNumber;
-//       console.log(secondNumber);
-//       console.log(currentTotal);
-//     }
-//     $('#screen').html(total);
-//     currentTotal = 0;
-//   }
-//   // var mathMethod;
-//   // function operators(event){
-//   //     if(this.innerHTML === '-'){
-//   //       mathMethod = 'subtraction';
-//   //     } else if(this.innerHTML === '+'){
-//   //       mathMethod = 'addition';
-//   //     } else if(this.innerHTML === 'x'){
-//   //       mathMethod = 'multiplication';
-//   //     } else if(this.innerHTML === 'C'){
-//   //       mathMethod = 'clear';
-//   //     } else if(this.innerHTML === '='){
-//   //       mathMethod = 'total';
-//   //     } else {
-//   //       mathMethod = 'division';
-//   //     }
-//   //   console.log(mathMethod);
-//   // }
-//   var operators = $('.operator');
-//
-//   function isNumber(event){
-//     if($.isNumeric(this.innerHTML)) {
-//       console.log('number!');
-//     } else{
-//         console.log('not a number!');
-//     }
-//   }
-//
-//   // function printToCalc(){
-//   //   this.innerHTML = currentTotal;
-//   // }
-// // $('#screen').html(currentTotal);
-//   // $('#screen').on('click', printToCalc);
-//
-//   // $('.operator').on('click', operators);
-//
-//   $('span').on('click', pickNumber);
-//   // $('span').on('click', isNumber);
-//
-// });
